a27b05b0abcd8965ddd49fe8930d08931fe75f66
Eruki <malasanedison1@gmail.com>
2025-04-14 22:01:34 +0800

    invoke AdminDAO methods to AdminController class

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -1,4 +1,31 @@
 package controllers;
 
+import DataAccessObject.AdminDAO;
+import models.Admin;
+
+import java.util.List;
+
+
 public class AdminController {
+    private AdminDAO adminDAO = new AdminDAO();
+
+    public AdminController() {
+        this.adminDAO = new AdminDAO();
+    }
+
+    private boolean createAdmin(Admin admin) {
+        return adminDAO.createAdmin(admin);
+    }
+
+    public Admin getAdminByVolunteerId(String volid) {
+        return adminDAO.getAdminByVolunteerId(volid);
+    }
+
+    public boolean updateAdmin(Admin admin) {
+        return adminDAO.updateAdmin(admin);
+    }
+
+    public List<Admin> getAllAdmins() {
+        return adminDAO.getAllAdmins();
+    }
 }
